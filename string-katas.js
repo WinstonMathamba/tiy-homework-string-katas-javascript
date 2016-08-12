35d9b3421c3103d5dda75fe72fcc3137780e81b1
WinBook <win808mac@Winstons-MacBook-2.local>
2016-08-12 14:24:47 -0400

    Wrapping up final touches

--- string-katas.js
+++ string-katas.js
@@ -129,15 +129,15 @@ Front22("strong");
 Front22("Future");
 
 /* Given a string, if the string "del" appears starting at index 1,
-//return a string where that "del" has been deleted. Otherwise,
-//return the string unchanged. */
+ //return a string where that "del" has been deleted. Otherwise,
+ //return the string unchanged. */
 
 function DelDel(str) {
-    if ((str.length >= 4) && (str.substring(1,4).localeCompare("del") == 0)) {
-        console.log("Example of DelDel --> " + str.slice(0,1) + str.slice(4));
+    if ((str.length >= 4) && (str.substring(1, 4).localeCompare("del") == 0)) {
+        console.log("Example of DelDel --> " + str.slice(0, 1) + str.slice(4));
 
-    }else {
-        console.log("Example of DelDel --> "+ str);
+    } else {
+        console.log("Example of DelDel --> " + str);
     }
 
 }
@@ -152,6 +152,49 @@ DelDel("SinglikeAdele");
 DelDel("idelnclude() isnt a valid function");
 
 
+/*Given a string, return a string made of the first 2 characters (if present),
+ //however include first char only if it is 'o' and include the second
+ //only if it is 'z', so "ozymandias" yields "oz". */
+
+function StartOz(str) {
+    var result = "";
+    if (str.length > 1 && (str.charAt(0) == 'o')) {
+        result = result + str.charAt(0);
+    }
+    if (str.length > 2 && (str.charAt(1) == 'z')) {
+        result = result + str.charAt(1);
+    }
+    console.log("Example of StartOz --> " + result);
+}
+
+console.log("");
+
+StartOz("ozymandias");
+StartOz("bzoo");
+StartOz("oxxx");
+StartOz("kitten");
+StartOz("loozy");
+StartOz("Ozzcar");
+
+function EndUp(str) {
+    if (str.length <= -3) {
+        console.log(str);
+    }
+    var last3 = str.length -3;
+    var front = str.slice(0,last3);
+    var back = str.slice(last3);
+
+    console.log("Example of EndUp --> " + front + back.toUpperCase());
+
+}
+
+console.log("");
+
+EndUp("hello");
+EndUp("hi there");
+EndUp("rooar");
+EndUp("whoop");
+EndUp("abc");
 
 
 
