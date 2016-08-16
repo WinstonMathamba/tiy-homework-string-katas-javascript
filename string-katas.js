ecdffae11478fbab6c5f0d769933708e6d6841a6
WinBook <win808mac@Winstons-MacBook-2.local>
2016-08-16 15:46:11 -0400

    Updates that include Doug's feedback

--- string-katas.js
+++ string-katas.js
@@ -10,155 +10,155 @@
  //spaces before the first character or after the last character. */
 
 
-function Explode(str) {
+function explode(str) {
 
-    console.log("Example of Explode --> " + str.split('').join(' '));
+    return str.split('').join(' ');
 }
 
-Explode("example");
-Explode("cat");
-Explode("c");
-Explode("wheel");
-Explode("wideload");
-Explode("rolling");
+console.log("Example of explode -->> ",explode("example"));
+console.log("Example of explode -->> ",explode("cat"));
+console.log("Example of explode -->> ",explode("c"));
+console.log("Example of explode -->> ",explode("wheel"));
+console.log("Example of explode -->> ",explode("wideload"));
+console.log("Example of explode -->> ",explode("rolling"));
 
 
 //* Given a string, return a new string where "not " has
 //been added to the front. However, if the string already begins with "not",
 //return the string unchanged. *!/*/
 
-function NotString(str) {
+function notString(str) {
 
     if (str.length >= 3 && str.startsWith("not")) {
-        console.log("Example of Notstring --> " + str);
+        return  str;
     }
     else {
-        console.log("Example of Notstring --> not " + str);
+        return "not " + str;
     }
 
 }
 console.log("");
 
-NotString("candy");
-NotString("silly");
-NotString("nottingham");
-NotString("belly");
-NotString("kettlebell");
-NotString("serious");
+console.log("Example of Notstring --> ",notString("candy"));
+console.log("Example of Notstring --> ",notString("silly"));
+console.log("Example of Notstring --> ",notString("nottingham"));
+console.log("Example of Notstring --> ",notString("belly"));
+console.log("Example of Notstring --> ",notString("kettlebell"));
+console.log("Example of Notstring --> ",notString("serious"));
 
 /* Missing Character - Given a non-empty string and an integer, return a new string where
  //the character at the specified index has been removed. The specified
  //index will be a valid index of in the original string.  */
 
-function MissingCharacter(str, n) {
+function missingCharacter(str, n) {
     var front = str.substring(0, n);
     var back = str.slice(n + 1);
 
 
-    console.log("Example of MissingCharacter --> " + front + back);
+    return front + back;
 
 }
 
 console.log("");
 
-MissingCharacter("Helium", 3);
-MissingCharacter("olympic", 2);
-MissingCharacter("serendipity", 5);
-MissingCharacter("platter", 1);
-MissingCharacter("healthy", 0);
-MissingCharacter("rarely", 4);
+console.log("Example of MissingCharacter --> ",missingCharacter("Helium", 3));
+console.log("Example of MissingCharacter --> ",missingCharacter("olympic", 2));
+console.log("Example of MissingCharacter --> ",missingCharacter("serendipity", 5));
+console.log("Example of MissingCharacter --> ",missingCharacter("platter", 1));
+console.log("Example of MissingCharacter --> ",missingCharacter("healthy", 0));
+console.log("Example of MissingCharacter --> ",missingCharacter("rarely", 4));
 
 /*Given a string, return a new string where the first and last characters
  //have been exchanged. */
 
-function FrontBack(str) {
+function frontBack(str) {
 
     if (str.length <= 1) console.log(str);
     var mid = str.slice(1, (str.length - 1));
 
-    console.log("Example of FrontBack --> " + str.charAt(str.length - 1) + mid + str.charAt(0))
+    return str.charAt(str.length - 1) + mid + str.charAt(0);
 
 
 }
 console.log("");
 
-FrontBack("Javascript");
-FrontBack("Chocolate");
-FrontBack("abc");
-FrontBack("Helium");
-FrontBack("settlement");
-FrontBack("progress");
+console.log("Example of FrontBack --> " ,frontBack("Javascript"));
+console.log("Example of FrontBack --> " ,frontBack("Chocolate"));
+console.log("Example of FrontBack --> " ,frontBack("abc"));
+console.log("Example of FrontBack --> " ,frontBack("Helium"));
+console.log("Example of FrontBack --> " ,frontBack("settlement"));
+console.log("Example of FrontBack --> " ,frontBack("progress"));
 
 /* Given a string, take the last character and return a new string
  //with the last character added at the front and back,
  //so "cat" yields "tcatt". The original string will be length 1 or more. */
 
-function BackAround(str) {
+function backAround(str) {
     var last = str.charAt(str.length - 1);
-    console.log("Example of BackAround --> " + last + str + last);
+    return last + str + last;
 
 }
 
 console.log("");
 
-BackAround("cat");
-BackAround("Hello");
-BackAround("a");
-BackAround("ohyeah");
-BackAround("RandomAccess");
-BackAround("everyday");
+console.log("Example of BackAround --> ", backAround("cat"));
+console.log("Example of BackAround --> ", backAround("Hello"));
+console.log("Example of BackAround --> ", backAround("a"));
+console.log("Example of BackAround --> ", backAround("ohyeah"));
+console.log("Example of BackAround --> ", backAround("RandomAccess"));
+console.log("Example of BackAround --> ", backAround("everyday"));
 
 /* Given a string, take the first 2 characters and return the string with the
  //2 characters added at both the front and back, so "kitten" yields"kikittenki".
  //If the string length is less than 2, use whatever characters are there. */
 
-function Front22(str) {
+function front22(str) {
     if (str.length >= 2) {
         var first2 = str.slice(0, 2);
-        console.log("Example of Front22 --> " + first2 + str + first2);
+        return first2 + str + first2;
     }
     else {
-        console.log("Example of Front22 --> " + str);
+        return str;
     }
 }
 
 console.log("");
-Front22("kitten");
-Front22("Ha");
-Front22("abc");
-Front22("carbon");
-Front22("strong");
-Front22("Future");
+console.log("Example of Front22 --> " ,front22("kitten"));
+console.log("Example of Front22 --> " ,front22("Ha"));
+console.log("Example of Front22 --> " ,front22("abc"));
+console.log("Example of Front22 --> " ,front22("carbon"));
+console.log("Example of Front22 --> " ,front22("strong"));
+console.log("Example of Front22 --> " ,front22("Future"));
 
 /* Given a string, if the string "del" appears starting at index 1,
  //return a string where that "del" has been deleted. Otherwise,
  //return the string unchanged. */
 
-function DelDel(str) {
+function delDel(str) {
     if ((str.length >= 4) && (str.substring(1, 4).localeCompare("del") == 0)) {
-        console.log("Example of DelDel --> " + str.slice(0, 1) + str.slice(4));
+        return str.slice(0, 1) + str.slice(4);
 
     } else {
-        console.log("Example of DelDel --> " + str);
+        return str;
     }
 
 }
 
 console.log("");
 
-DelDel("adelbc");
-DelDel("hellod");
-DelDel("Hellodel");
-DelDel("Adelesings")
-DelDel("SinglikeAdele");
-DelDel("idelnclude() isnt a valid function");
+console.log("Example of DelDel --> " ,delDel("adelbc"));
+console.log("Example of DelDel --> " ,delDel("hellod"));
+console.log("Example of DelDel --> " ,delDel("Hellodel"));
+console.log("Example of DelDel --> " ,delDel("Adelesings"));
+console.log("Example of DelDel --> " ,delDel("SinglikeAdele"));
+console.log("Example of DelDel --> " ,delDel("idelnclude() isnt a valid function"));
 
 
 /*Given a string, return a string made of the first 2 characters (if present),
  //however include first char only if it is 'o' and include the second
  //only if it is 'z', so "ozymandias" yields "oz". */
 
-function StartOz(str) {
+function startOz(str) {
     var result = "";
     if (str.length > 1 && (str.charAt(0) == 'o')) {
         result = result + str.charAt(0);
@@ -166,19 +166,19 @@ function StartOz(str) {
     if (str.length > 2 && (str.charAt(1) == 'z')) {
         result = result + str.charAt(1);
     }
-    console.log("Example of StartOz --> " + result);
+    return result;
 }
 
 console.log("");
 
-StartOz("ozymandias");
-StartOz("bzoo");
-StartOz("oxxx");
-StartOz("kitten");
-StartOz("loozy");
-StartOz("Ozzcar");
+console.log("Example of StartOz --> " ,startOz("ozymandias"));
+console.log("Example of StartOz --> " ,startOz("bzoo"));
+console.log("Example of StartOz --> " ,startOz("oxxx"));
+console.log("Example of StartOz --> " ,startOz("kitten"));
+console.log("Example of StartOz --> " ,startOz("loozy"));
+console.log("Example of StartOz --> " ,startOz("Ozzcar"));
 
-function EndUp(str) {
+function endUp(str) {
     if (str.length <= -3) {
         console.log(str);
     }
@@ -186,17 +186,17 @@ function EndUp(str) {
     var front = str.slice(0, last3);
     var back = str.slice(last3);
 
-    console.log("Example of EndUp --> " + front + back.toUpperCase());
+    return front + back.toUpperCase();
 
 }
 
 console.log("");
 
-EndUp("hello");
-EndUp("hi there");
-EndUp("rooar");
-EndUp("whoop");
-EndUp("abc");
+console.log("Example of EndUp --> ", endUp("hello"));
+console.log("Example of EndUp --> ", endUp("hi there"));
+console.log("Example of EndUp --> ", endUp("rooar"));
+console.log("Example of EndUp --> ", endUp("whoop"));
+console.log("Example of EndUp --> ", endUp("abc"));
 
 
 
